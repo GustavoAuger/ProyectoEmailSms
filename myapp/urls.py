10d59661180e2a36be356097db5a182ee50ff809
Gustavo Auger <135929612+GustavoAuger@users.noreply.github.com>
2024-11-06 00:26:02 -0300

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
 ]
 
 urlpatterns = [
-
+    path('', views.index, name='index'),  # Ruta para la URL raíz
     path('index', views.index, name='index'),
     path('base', views.base, name='base'),
     path('contacto', views.contacto, name='contacto'),
