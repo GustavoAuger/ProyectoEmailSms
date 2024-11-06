0d329850604061211d3f4a68078cab4b53b97b82
carlhidalgo <106288856+carlhidalgo@users.noreply.github.com>
2024-11-06 01:55:09 -0300

    Update manage.py to set project settings and automate migrations

--- manage.py
+++ manage.py
@@ -3,19 +3,18 @@
 import os
 import sys
 from django.core.wsgi import get_wsgi_application
-
+import django
+from django.core.management import execute_from_command_line
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WebService.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tu_proyecto.settings')
+    django.setup()
     try:
-        from django.core.management import execute_from_command_line
-    except ImportError as exc:
-        raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+        # Ejecuta las migraciones automáticamente
+        execute_from_command_line(['manage.py', 'migrate'])
+    except Exception as exc:
+        print(f"Error al ejecutar migraciones: {exc}")
+    # Ejecuta el servidor
     execute_from_command_line(sys.argv)
 
 
